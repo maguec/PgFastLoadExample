2f23cae60dfa8b7f07a7a3de9467f51db0b7d6c8
Chris Mague <github@mague.com>
2025-12-11 12:59:05 -0800

    add more sample data

--- data_gen.py
+++ data_gen.py
@@ -2,8 +2,14 @@ from faker import Faker
 import json
 
 fake = Faker()
-data =  []
-for _ in range(1_000):
-    data.append({"first_name": fake.first_name(), "last_name": fake.last_name(), "email": fake.email()})
+data = []
+for _ in range(1_000_000):
+    data.append(
+        {
+            "first_name": fake.first_name(),
+            "last_name": fake.last_name(),
+            "email": fake.email(),
+        }
+    )
 with open("data.json", "w") as f:
-        f.write( json.dumps(data))
+    f.write(json.dumps(data))
